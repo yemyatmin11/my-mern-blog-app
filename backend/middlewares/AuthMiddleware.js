53fa093ce43c29a93a75d8b92595e1206897ca73
Ye Myat Min <153253878+yemyatmin11@users.noreply.github.com>
2024-07-18 19:52:34 +0630

    Update AuthMiddleware.js

--- backend/middlewares/AuthMiddleware.js
+++ backend/middlewares/AuthMiddleware.js
@@ -3,21 +3,24 @@ const User = require('../Models/User');
 
 const AuthMiddleware = (req, res, next) => {
     let token = req.cookies.jwt;
-    if(token) {
+    if (token) {
         jwt.verify(token, process.env.JWT_SECRET, (err, decodedValue) => {
-            if(err) {
-                return res.status(401).json( { message : 'unauthenticated' });
-            }
-            else {
+            if (err) {
+                console.error('Token verification failed:', err);
+                return res.status(401).json({ message: 'unauthenticated' });
+            } else {
                 User.findById(decodedValue._id).then(user => {
                     req.user = user;
                     next();
-                })    
+                }).catch(err => {
+                    console.error('User retrieval failed:', err);
+                    return res.status(500).json({ message: 'internal server error' });
+                });
             }
-        })
-    }
-    else {
-        return res.status(400).json( { message : 'token need to provide' });
+        });
+    } else {
+        console.error('Token not provided');
+        return res.status(400).json({ message: 'token need to provide' });
     }
 }
 
